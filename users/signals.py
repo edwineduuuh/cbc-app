be0d5eda4c1ecc699f9597b0f91ad2810bb4c89e
syntax-elpadrino <eduardoeduuh@proton.me>
2026-04-12 22:20:45 +0300

    Fix registration crash: remove broken post_save signal calling deleted activate_free_trial()

--- users/signals.py
+++ users/signals.py
@@ -1,24 +1,3 @@
-# AUTO-ACTIVATE FREE TRIAL ON REGISTRATION
-# Add this to users/signals.py (create if doesn't exist)
-
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-@receiver(post_save, sender=User)
-def activate_trial_on_signup(sender, instance, created, **kwargs):
-    """Auto-grant 7-day trial when user registers"""
-    if created and instance.role == 'student':  # Only for students
-        instance.activate_free_trial()
-
-
-# THEN in users/apps.py, make sure signals are loaded:
-# 
-# class UsersConfig(AppConfig):
-#     default_auto_field = 'django.db.models.BigAutoField'
-#     name = 'users'
-#     
-#     def ready(self):
-#         import users.signals  # Import signals
+# User signals
+# Note: Free trial credits (quiz_credits=4) are set by the model default.
+# No post_save signal needed.
